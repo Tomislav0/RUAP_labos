1d35e21174c4ac941c94112da7e758b77020ed14
Krcmar <tea.krcmar@gmail.com>
2022-10-27 17:28:09 +0200

    added index number

--- RUAP/RUAP/Program.cs
+++ RUAP/RUAP/Program.cs
@@ -6,7 +6,7 @@ namespace RUAP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine( "Pozdrav iz aplikacije od Tomislava Kovacevica");
+            Console.WriteLine( "Pozdrav iz aplikacije od Tomislava Kovacevica [broj indexa]");
         }
     }
 }
